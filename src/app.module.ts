58a12f6535296ae72051d99ccfe67ed3ef5da0ea
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-01-08 14:02:53 +0400

    mongo connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersRepository } from './users/users.repository';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://miha:miha2016!@cluster0.expiegq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+  ], //[MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING)],
+  //forRoot('mongodb://localhost/nest')],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService, UsersRepository],
 })
